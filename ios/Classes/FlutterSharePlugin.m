dbf250461c96a572affc13a54b12ab0f8a400abf
lucas.britto <l_demello@hotmail.com>
2019-09-04 20:44:59 -0300

    Remove swift file import and get top most view controller

--- ios/Classes/FlutterSharePlugin.m
+++ ios/Classes/FlutterSharePlugin.m
@@ -1,29 +1,19 @@
 #import "FlutterSharePlugin.h"
-#import <flutter_share/flutter_share-Swift.h>
 
 @interface FlutterSharePlugin()
 @end
 
 @implementation FlutterSharePlugin{
     FlutterResult _result;
-    UIViewController *_viewController;
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
                                        methodChannelWithName:@"flutter_share"
                                        binaryMessenger:[registrar messenger]];
-  UIViewController *viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-  FlutterSharePlugin *instance = [[FlutterSharePlugin alloc] initWithViewController:viewController];
-  [registrar addMethodCallDelegate:instance channel:channel];
-}
 
-- (instancetype)initWithViewController:(UIViewController *)viewController {
-    self = [super init];
-    if (self) {
-        _viewController = viewController;
-    }
-    return self;
+  FlutterSharePlugin *instance = [FlutterSharePlugin alloc];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -43,41 +33,41 @@
     NSString *text = call.arguments[@"text"];
     NSString *linkUrl = call.arguments[@"linkUrl"];
 
-    if (title == nil || [title length] == 0) {
+    if ([title isEqual:[NSNull null]] || [title length] == 0) {
       return false;
     }
 
     NSMutableArray *sharedItems = [NSMutableArray array];
     NSMutableArray *textList = [NSMutableArray array];
-    
+
     // text
-    if (text != nil && [text length] > 0) {
+    if (![text isEqual:[NSNull null]] && [text length] > 0) {
        [textList addObject:text];
     }
     // Link url
-    if (linkUrl != nil && [linkUrl length] > 0) {
+    if (![linkUrl isEqual:[NSNull null]] && [linkUrl length] > 0) {
         [textList addObject:linkUrl];
     }
-    
+
     NSString *textToShare = @"";
-    
+
     if ([textList count] > 0) {
         textToShare = [textList componentsJoinedByString:@"\n\n"];
     }
-    
+
     [sharedItems addObject:textToShare];
-    
+
     UIActivityViewController *activityViewController = [[UIActivityViewController alloc]initWithActivityItems:sharedItems applicationActivities:nil];
-    
+
     // Subject
-    if (title != nil && [title length] > 0) {
+    if (![title isEqual:[NSNull null]] && [title length] > 0) {
         [activityViewController setValue:title forKey:@"subject"];
     }
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self->_viewController presentViewController:activityViewController animated:YES completion:nil];
+        [[self topViewController] presentViewController:activityViewController animated:YES completion:nil];
     });
-    
+
     return true;
 }
 
@@ -85,36 +75,66 @@
     NSString *title = call.arguments[@"title"];
     NSString *text = call.arguments[@"text"];
     NSString *filePath = call.arguments[@"filePath"];
-    
-    if (title == nil || [title length] == 0 || filePath == nil || [filePath length] == 0) {
+
+    if ([title isEqual:[NSNull null]] || [title length] == 0 || [filePath isEqual:[NSNull null]] || [filePath length] == 0) {
         return false;
     }
-    
+
     NSMutableArray *sharedItems = [NSMutableArray array];
-    
+
     // text
-    if (text != nil && [text length] > 0) {
+    if (![text isEqual:[NSNull null]] && [text length] > 0) {
         [sharedItems addObject:text];
     }
-    
+
     // Link url
-    if (filePath != nil && [filePath length] > 0) {
+    if (![filePath isEqual:[NSNull null]] && [filePath length] > 0) {
         NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
         [sharedItems addObject:fileUrl];
     }
-    
+
     UIActivityViewController *activityViewController = [[UIActivityViewController alloc]initWithActivityItems:sharedItems applicationActivities:nil];
-    
+
     // Subject
-    if (title != nil && [title length] > 0) {
+    if (![title isEqual:[NSNull null]] && [title length] > 0) {
         [activityViewController setValue:title forKey:@"subject"];
     }
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self->_viewController presentViewController:activityViewController animated:YES completion:nil];
+        [[self topViewController] presentViewController:activityViewController animated:YES completion:nil];
     });
-    
+
     return true;
+}
+
+- (UIViewController*)topViewController {
+    return [self topViewControllerWithRootViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+}
+
+- (UIViewController*)topViewControllerWithRootViewController:(UIViewController*)viewController {
+    if ([viewController isKindOfClass:[UITabBarController class]]) {
+        UITabBarController* tabBarController = (UITabBarController*)viewController;
+        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
+    } else if ([viewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController* navContObj = (UINavigationController*)viewController;
+        return [self topViewControllerWithRootViewController:navContObj.visibleViewController];
+    } else if (viewController.presentedViewController && !viewController.presentedViewController.isBeingDismissed) {
+        UIViewController* presentedViewController = viewController.presentedViewController;
+        return [self topViewControllerWithRootViewController:presentedViewController];
+    }
+    else {
+        for (UIView *view in [viewController.view subviews])
+        {
+            id subViewController = [view nextResponder];
+            if ( subViewController && [subViewController isKindOfClass:[UIViewController class]])
+            {
+                if ([(UIViewController *)subViewController presentedViewController]  && ![subViewController presentedViewController].isBeingDismissed) {
+                    return [self topViewControllerWithRootViewController:[(UIViewController *)subViewController presentedViewController]];
+                }
+            }
+        }
+        return viewController;
+    }
 }
 
 @end
